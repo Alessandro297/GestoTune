f802743c0678813d59075b7ac323e652de18e9f5
Alessandro Piani <64686320+Alessandro297@users.noreply.github.com>
2024-02-18 00:48:00 +0100

    updated constants

--- constants.py
+++ constants.py
@@ -6,7 +6,5 @@ TOKEN_URL = "https://accounts.spotify.com/api/token"
 # Scopes provide Spotify users using third-party apps the confidence that only the information they choose to share will be shared.
 SCOPE = "user-read-currently-playing user-read-playback-state user-modify-playback-state playlist-read-private playlist-modify-private user-library-modify user-library-read"
 
-THRESHOLD = 0.900
-ACTIONS = np.array(["play", "pause", "increase", "decrease", "next", "previous"])
-
-# DATA_PATH = ...
+THRESHOLD = 0.995
+ACTIONS = np.array(["previous", "play", "next", "pause", "decrease", "increase"])
