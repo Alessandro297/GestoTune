e9123856a4800d78c54afd49b37b89785797e5ad
Alessandro Piani <64686320+Alessandro297@users.noreply.github.com>
2024-02-19 11:21:54 +0100

    uploaded main

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication
-
+from PyQt5.QtCore import Qt
 from GUI.MainWindow import MainWindow
 
 def main():
+    # Handle high resolution displays:
+    if hasattr(Qt, 'AA_EnableHighDpiScaling'):
+        QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+    if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
+        QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+
     application = QApplication([])
     window = MainWindow()
     window.show()
